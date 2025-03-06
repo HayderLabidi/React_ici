7953ff14d6e807a70bdc543323df6bf053b666f1
Hayder Labidi <108490838+HayderLabidi@users.noreply.github.com>
2025-03-06 12:00:18 +0100

    v8

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import Product from '../components/Product'
-import data from '../Data'
+import React, { useEffect, useState } from "react";
+import Product from "../components/Product";
+import axios from "axios";
 
 export default function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("http://localhost:5000/api/products");
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
-          <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
-          </div>
+      <div>
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
         </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,71 +1,58 @@
 import React from 'react';
 import data from '../Data';
-import { useParams , Link} from 'react-router-dom';
+import { useParams, Link } from "react-router-dom";
 import Rating from '../components/Rating';
 
 export default function ProductScreen() {
-    const { id } = useParams();
-    const product = data.products.find((x) => x._id === Number(id) );
-    return (
-      <div>
-        <Link to='/'>Back to result</Link>
-        <div className="row top">
-            <div className="col-2">
-                <img className="large" src={product.image} alt={product.name} />
-                <div className="col-1">
-                  <ul>
-                    <li>
-                      <h1>{product.name}</h1>
-                    </li>
-                    <li>
-                      <Rating rating={product.rating}></Rating>
-                    </li>
-                    <li>
-                      Price: ${product.price}
-                    </li>
-                    <li>
-                      Category: {product.category}
-                    </li>
-                    <li>
-                      Description: <p>{product.description}</p>
-                    </li>
-                  </ul>
+  const { id } = useParams();
+  const product = data.products.find((x) => x._id === Number(id));
+  return (
+    <div>
+      <Link to="/"> Back to result</Link>
+      <div className='row top'>
+        <div className='col-2'>
+          <img className='large' src={product.image} alt={product.name}></img>
+        </div>
+        <div className='col-1'>
+          <ul>
+            <li>
+              <h1>{product.name}</h1>
+            </li>
+            <li>
+              <Rating rating={product.rating}></Rating>
+            </li>
+            <li>Price: ${product.price}</li>
+            <li>Description : <p>{product.description}</p></li>
+          </ul>
+        </div>
+        <div className='col-1'>
+          <div className='card card-body'>
+            <ul>
+              <li>
+                <div className='row'>
+                  <div>Price </div>
+                  <div className='price'>${product.price}</div>
                 </div>
-                <div className='col-1'>
-                  <div className='card card-body'>
-                    <ul>
-                      <li>
-                        <div className='row'> 
-                          <div>
-                            Price:
-                          </div>
-                          <div className='price'>
-                            ${product.price}
-                          </div>
-                        </div>
-                      </li>
-                      <li>
-                        <div className='row'>
-                          <div>Status: </div>
-                          <div>
-                            {/* TERNER  === if else */}
-                            {product.CountInStock > 0 ?(
-                              <span className='success'>In stock</span>
-                            ):(
-                              <span className='failed'>Unvaialble</span>
-                            )
-                          }
-                          </div>
-                        </div>
-                      </li>
-                      <li>
-                        <button className='primary block'></button>
-                      </li>
-                    </ul>
+              </li>
+              <li>
+                <div className='row'>
+                  <div>Status</div>
+                  <div>
+                    {product.CountInStock > 0 ? (
+                      <span className='success'>In Stock</span>
+                    ) : (
+                      <span className='error'>Unavailable</span>
+                    )}
                   </div>
                 </div>
-            </div>
+              </li>
+              <li>
+                <button className='primary block'>Add to cart</button>
+              </li>
+            </ul>
+          </div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
